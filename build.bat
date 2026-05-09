e1bd5924aea5d6b5bfb0421f1aef79edaba929ec
deanjiang <1125414212@qq.com>
2026-05-09 19:50:46 +0800

    chore: 清理根目录无用文件
    - 删除临时测试脚本 (test*.py, debug_menu.py)
    - 删除构建脚本 (build*.bat)
    - 删除过时文档 (PRODUCT.md, RELEASE_NOTES.md, 鹅宝_学术论文.md 等)
    - 删除分析配置 (analysis_options.yaml)
    - 删除 PPT 产物

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d e:\baby\GooseBaby
-set PUB_HOSTED_URL=https://pub.flutter-io.cn
-set FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
-e:\flutter324\flutter\bin\flutter.bat build windows
